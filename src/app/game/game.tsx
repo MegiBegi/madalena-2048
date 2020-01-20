09b04babfd43e0fcd9af27994f80fb24a87392de
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2020-01-20 21:58:06 +0100

    added ref to grid

--- src/app/game/game.tsx
+++ src/app/game/game.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useEffect, useState } from "react"
+import React, { FC, ReactElement, useEffect, useState, useRef } from "react"
 import { connect } from "react-redux"
 import { useSwipeable } from "react-swipeable"
 import * as actions from "redux/actions"
@@ -68,6 +68,7 @@ const Game: FC<GameStateProps & DispatchProps> = ({
   const [zoom, setZoom] = useState<boolean>(false)
   const [create, setCreate] = useState<string>("create")
   const [merge, setMerge] = useState<string>("merge")
+  const gridRef = useRef<HTMLDivElement>(null)
 
   const updatedGrid: TileInfo[] = updateGrid(numbers)
 
@@ -141,6 +142,9 @@ const Game: FC<GameStateProps & DispatchProps> = ({
     }
 
     window.addEventListener("keydown", handleKeyPress)
+    gridRef?.current?.addEventListener("touchmove", e => {
+      e.preventDefault()
+    })
 
     return (): void => {
       window.removeEventListener("keydown", handleKeyPress)
@@ -184,7 +188,7 @@ const Game: FC<GameStateProps & DispatchProps> = ({
           <Score disabled>Best score: {bestScore}</Score>
         </Header>
         <Main>
-          <Grid zoomIn={zoom}>
+          <Grid ref={gridRef} zoomIn={zoom}>
             {gridItems}
             <Zoom viewBox="0 0 20 20" onClick={(): void => zooming()}>
               <path d="M18.125,15.804l-4.038-4.037c0.675-1.079,1.012-2.308,1.01-3.534C15.089,4.62,12.199,1.75,8.584,1.75C4.815,1.75,1.982,4.726,2,8.286c0.021,3.577,2.908,6.549,6.578,6.549c1.241,0,2.417-0.347,3.44-0.985l4.032,4.026c0.167,0.166,0.43,0.166,0.596,0l1.479-1.478C18.292,16.234,18.292,15.968,18.125,15.804 M8.578,13.99c-3.198,0-5.716-2.593-5.733-5.71c-0.017-3.084,2.438-5.686,5.74-5.686c3.197,0,5.625,2.493,5.64,5.624C14.242,11.548,11.621,13.99,8.578,13.99 M16.349,16.981l-3.637-3.635c0.131-0.11,0.721-0.695,0.876-0.884l3.642,3.639L16.349,16.981z" />
